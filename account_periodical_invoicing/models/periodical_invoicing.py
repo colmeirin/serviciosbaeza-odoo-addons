69ec9cb26540bd755e79fbf516220c033b763f92
Javier Colmeiro <javier@javiercolmeiro.com>
2015-08-05 09:37:31 +0200

    + remove adding Period text to product description

--- account_periodical_invoicing/models/periodical_invoicing.py
+++ account_periodical_invoicing/models/periodical_invoicing.py
@@ -417,9 +417,9 @@ class Agreement(orm.Model):
                     next_invoice_date, agreement_line.invoicing_unit,
                     agreement_line.invoicing_interval)
                 to_date = next_invoice_date - timedelta(days=1)
-            invoice_line['name'] += "\n" + _('Period: from %s to %s') % (
-                from_date.strftime(lang.date_format),
-                to_date.strftime(lang.date_format))
+            # invoice_line['name'] += "\n" + _('Period: from %s to %s') % (
+            #     from_date.strftime(lang.date_format),
+            #     to_date.strftime(lang.date_format))
             invoice_lines_vals.append(invoice_line)
             agreement_lines_ids.append(agreement_line.id)
         key = (agreement.partner_id.id, agreement.currency_id.id)
